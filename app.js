d80ed9f9c78a68419755e29200e8814e5e3b3403
techdave <klatzdavie19@gmail.com>
2022-05-04 05:40:11 +0300

    set up express

---
+++ app.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+
+// import routes
+const userRoutes = require('./routes/user');
+
+// app
+const app = express();
+
+// db
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreatedIndex: true,
+  })
+  .then(() => console.log('DB Connection established'));
+
+// routes
+app.use('/api/v1', userRoutes);
+
+// listen
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
+});
