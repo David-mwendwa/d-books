e4f5233cef2483cafc5d419107e7bee201f3d480
techdave <klatzdavie19@gmail.com>
2022-05-05 07:18:53 +0300

    create _create product_ route with file upload

---
+++ controllers/product.js
@@ -0,0 +1,30 @@
+const { StatusCodes } = require('http-status-codes');
+const formidable = require('formidable');
+const _ = require('lodash');
+const fs = require('fs');
+const Product = require('../models/product');
+const { errorHandler } = require('../utils/dbErrorHandler');
+
+exports.create = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ error: 'Image could not be uploaded' });
+    }
+    const product = new Product(fields);
+    if (files.photo) {
+      product.photo.data = fs.readFileSync(files.photo.path);
+      product.photo.contentType = files.photo.type;
+    }
+
+    product.save((err, result) => {
+      if (err) {
+        res.status(StatusCodes.BAD_REQUEST).json({ error: errorHandler(err) });
+      }
+      res.status(StatusCodes.CREATED).json(result);
+    });
+  });
+};
