fd61e548b0a65c527f4ba17201216e51dbf3ecd3
techdave <klatzdavie19@gmail.com>
2022-05-08 07:18:35 +0300

    handle _category create success and error messages

--- client/src/admin/AddCategory.js
+++ client/src/admin/AddCategory.js
@@ -1,10 +1,12 @@
 import React, { useState } from 'react';
 import Layout from '../core/Layout';
 import { isAuthenticated } from '../auth';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { logGreeting } from '../utils/logGreeting';
+import { createCategory } from './apiAdmin';
 
 const AddCategory = () => {
+  const navigate = useNavigate();
   const [name, setName] = useState('');
   const [error, setError] = useState(false);
   const [success, setSuccess] = useState(false);
@@ -22,7 +24,38 @@ const AddCategory = () => {
     setError('');
     setSuccess(false);
     // make request to api to create catergory
+    createCategory(user._id, token, { name }).then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setError('');
+        setSuccess(true);
+        setTimeout(() => navigate('/admin/dashboard'), 2000);
+      }
+    });
   };
+
+  const showSuccess = () => {
+    if (success) {
+      return (
+        <p className='alert alert-success'>New category, {name}, is created</p>
+      );
+    }
+  };
+
+  const showError = () => {
+    if (error) {
+      return <p className='alert alert-danger'>Category should be unique</p>;
+    }
+  };
+
+  const goBack = () => (
+    <div className='mt-5'>
+      <Link to='/admin/dashboard' className='text-warning'>
+        Back to Dashboard
+      </Link>
+    </div>
+  );
 
   const newCategoryFrom = () => (
     <form onSubmit={clickSubmit}>
@@ -47,7 +80,12 @@ const AddCategory = () => {
       title='Add a new category'
       description={`${logGreeting(user.name)}. Ready to add a new category?`}>
       <div className='row'>
-        <div className='col-md-8 offset-md-2'>{newCategoryFrom()}</div>
+        <div className='col-md-8 offset-md-2'>
+          {showSuccess()}
+          {showError()}
+          {newCategoryFrom()}
+          {goBack()}
+        </div>
       </div>
     </Layout>
   );
