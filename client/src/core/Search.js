2401c8b2dcd950ec2e84335003dd415acd02bb4d
techdave <klatzdavie19@gmail.com>
2022-05-13 08:16:56 +0300

    build search form

--- client/src/core/Search.js
+++ client/src/core/Search.js
@@ -28,7 +28,45 @@ const Search = () => {
     loadCategories();
   }, []);
 
-  return <div>Search { JSON.stringify(categories)}</div>;
+  const handleChange = () => {};
+
+  const searchSubmit = () => {};
+
+  return (
+    <div className='row'>
+      <div className='container mb-3'>
+        <form onSubmit={searchSubmit}>
+          <span className='input-group-text'>
+            <div className='input-group input-group-lg'>
+              <div className='input-group-prepend'>
+                <select
+                  onChange={handleChange('category')}
+                  className='btn mr-2'
+                  id=''>
+                  <option value='All'>Pick Category</option>
+                  {categories.map((c, i) => (
+                    <option key={i} value={c._id}>
+                      {c.name}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <input
+                type='search'
+                style={{borderRadius: '4px'}}
+                className='form-control'
+                placeholder='Search by name'
+                onChange={handleChange('search')}
+              />
+            </div>
+            <div className='btn input-group-append' style={{ border: 'none' }}>
+              <button className='input-group-text'>Search</button>
+            </div>
+          </span>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
